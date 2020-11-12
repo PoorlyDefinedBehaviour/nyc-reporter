3dcad5f65c2009686e9c3ca5e0374e3d0b0b7428
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-11-11 21:21:24 -0300

    test

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,30 @@ import * as core from "@actions/core"
 import * as github from "@actions/github"
 
 const main = async () => {
-  if (!github.context.payload.pull_request) {
-    return
+  try {
+    if (!github.context.payload.pull_request) {
+      return
+    }
+    // `who-to-greet` input defined in action metadata file s
+    const nameToGreet = core.getInput("who-to-greet")
+    const githubToken = core.getInput("GITHUB_TOKEN")
+    console.log(`Hello ${nameToGreet}!`)
+    const time = new Date().toTimeString()
+    core.setOutput("time", time)
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`)
+
+    const octokit = github.getOctokit(githubToken)
+
+    await octokit.issues.createComment({
+      ...github.context.repo,
+      issue_number: github.context.payload.pull_request.number,
+      body: "hello world",
+    })
+  } catch (error) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    core.setFailed(error.message)
   }
-  // `who-to-greet` input defined in action metadata file s
-  const nameToGreet = core.getInput("who-to-greet")
-  const githubToken = core.getInput("GITHUB_TOKEN")
-  console.log(`Hello ${nameToGreet}!`)
-  const time = new Date().toTimeString()
-  core.setOutput("time", time)
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`)
-
-  const octokit = github.getOctokit(githubToken)
-
-  await octokit.issues.createComment({
-    ...github.context.repo,
-    issue_number: github.context.payload.pull_request.number,
-    body: "hello world",
-  })
 }
 main().catch((error: Error) => core.setFailed(error.message))
