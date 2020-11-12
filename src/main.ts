eed6ac5dac01b291b329329ba381a1aac217f4a9
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-11-11 23:15:35 -0300

    v1

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const getCoverageOutputTextForCommand = async (command: string) => {
   let outputText = ""
 
   await exec.exec(
-    `node_modules/.bin/nyc --reporter=lcov --reporter=text-summary ${command}`,
+    `yarn nyc --reporter=lcov --reporter=text-summary ${command}`,
     undefined,
     {
       listeners: {
