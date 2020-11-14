e2d7ad080aa0199fa25b4b5fe3a2ae0b895a0bb8
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-11-14 19:03:52 -0300

    refactor: extract pull request comment logic to a function

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,22 @@ const getCoverageOutputTextForCommand = async (command: string) => {
   return text.slice(reportStartsAt)
 }
 
+const commentOnPullRequest = async (comment: string) => {
+  if (!github.context.payload.pull_request) {
+    throw new Error("No pull request found")
+  }
+
+  const githubToken = core.getInput("GITHUB_TOKEN")
+
+  const octokit = github.getOctokit(githubToken)
+
+  await octokit.issues.createComment({
+    ...github.context.repo,
+    issue_number: github.context.payload.pull_request.number,
+    body: comment,
+  })
+}
+
 const main = async () => {
   if (!github.context.payload.pull_request) {
     return
@@ -39,15 +55,7 @@ const main = async () => {
 
   const coverageOutputText = await getCoverageOutputTextForCommand(command)
 
-  const githubToken = core.getInput("GITHUB_TOKEN")
-
-  const octokit = github.getOctokit(githubToken)
-
-  await octokit.issues.createComment({
-    ...github.context.repo,
-    issue_number: github.context.payload.pull_request.number,
-    body: coverageOutputText,
-  })
+  await commentOnPullRequest(coverageOutputText)
 }
 
 main().catch((error: Error) => core.setFailed(error.message))
